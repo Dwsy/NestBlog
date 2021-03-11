b9924c53d68da8cfcc17db6e06ab0bf6aca1405f
dwsy <1521986032@qq.com>
2021-03-11 14:22:32 +0800

    1

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-// import { Log4jsLogger } from '@nestx-log4js/core';
-import * as express from 'express';
-import { logger } from './middleware/logger.middleware';
-
 // import * as mongoose from 'mongoose';
 async function bootstrap() {
   // mongoose.connect('mogodb://localhost:27017')
@@ -20,6 +15,7 @@ app.enableCors();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
   app.enableCors();
+<<<<<<< HEAD
 
   // app.useLogger(app.get(Log4jsLogger));
 
@@ -29,6 +25,8 @@ app.enableCors();
   app.use(logger);
   
 
+=======
+>>>>>>> parent of 89922a6 (log)
   const PORT = process.env.ADMIN_PORT || 2999
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}/api-docs`)
