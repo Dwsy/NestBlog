7fafacf40f900fdc094a45e0abd6c5d681067a9c
dwsy <1521986032@qq.com>
2021-03-11 14:23:34 +0800

    Update main.ts

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// import { Log4jsLogger } from '@nestx-log4js/core';
+import * as express from 'express';
+import { logger } from './middleware/logger.middleware';
+
 // import * as mongoose from 'mongoose';
 async function bootstrap() {
   // mongoose.connect('mogodb://localhost:27017')
@@ -15,18 +20,6 @@ app.enableCors();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
   app.enableCors();
-<<<<<<< HEAD
-
-  // app.useLogger(app.get(Log4jsLogger));
-
-  app.use(express.json()); // For parsing application/json
-  app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-  // 监听所有的请求路由，并打印日志
-  app.use(logger);
-  
-
-=======
->>>>>>> parent of 89922a6 (log)
   const PORT = process.env.ADMIN_PORT || 2999
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}/api-docs`)
