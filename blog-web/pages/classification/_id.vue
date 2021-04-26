f752b5d551bf55487d41112dec0002e2a585a6ca
dwsy <1521986032@qq.com>
2021-04-26 20:11:39 +0800

    mm

--- blog-web/pages/classification/_id.vue
+++ blog-web/pages/classification/_id.vue
@@ -1,22 +1,23 @@
 <template>
-  <v-container grid-list-xs>
-    <h1>当前分类：{{fields[0].classification.name}}</h1>
-    <v-divider></v-divider>
-    <ArticleList :fields="fields" />
-  </v-container>
+    <v-container grid-list-xs>
+        <h1>当前分类：{{ fields[0].classification.name }}</h1>
+        <v-divider></v-divider>
+        <ArticleList :fields="fields" />
+    </v-container>
 </template>
 
 <script>
 import ArticleList from "../../components/article/ArticleList";
 export default {
-  async asyncData({ $axios, params }) {
-    const id = params.id;
-    // console.log(id);
-    const fields = await $axios.$get(`classification/article/${id}`);
-
-    return { fields };
-  },
-  components: { ArticleList }
+    async asyncData({ $axios, params }) {
+        const id = params.id;
+        // console.log(id);
+        const fields = await $axios.$get(`classification/article/${id}`);
+        
+        console.log(fields);
+        return { fields };
+    },
+    components: { ArticleList }
 };
 </script>
 
